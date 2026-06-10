cecfdf6907f5681a8f922fa294084bbdca53afb6
AJ Thomas <1ajthomas248@.com>
2026-06-10 14:19:53 -0500

    refactor(networking): build VNet subnets and NSGs from inputs

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,128 +1,147 @@
-resource "azurerm_virtual_network" "enterprise-vnet" {
-  name = "enterprise-vnet"
-  resource_group_name = azurerm_resource_group.azure_enterprise_project.name
-  location = azurerm_resource_group.azure_enterprise_project.location
+resource "azurerm_virtual_network" "enterprise_vnet" {
+  name                = var.vnet.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = var.vnet.address_space
 
-  address_space = [var.address_space]
+  tags = local.tags
+}
 
-  subnet {
-    name = "vm-subnet"
-    address_prefixes = [var.address_prefix[0]]
-  }
+resource "azurerm_subnet" "this" {
+  for_each = var.subnets
 
-  subnet {
-    name = "app-subnet"
-    address_prefixes = [var.address_prefix[1]]
-  }
-
-  subnet {
-    name = "priv-endpoint-subnet"
-    address_prefixes = [var.address_prefix[2]]
-  }
-
-  subnet {
-    name = "appgw-subnet"
-    address_prefixes = [var.address_prefix[3]]
-  }
-
-  subnet {
-    name = "AzureBastionSubnet"
-    address_prefixes = [var.address_prefix[4]]
-  }
+  name                 = each.value.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.enterprise_vnet.name
+  address_prefixes     = each.value.address_prefixes
 }
 
 resource "azurerm_network_security_group" "app_nsg" {
-  name = "app_nsg"
-  resource_group_name = azurerm_resource_group.azure_enterprise_project.name
-  location = azurerm_resource_group.azure_enterprise_project.location
+  for_each = var.subnets["app"].nsg_enabled ? { app = var.subnets["app"] } : {}
+
+  name                = "app_nsg"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = local.tags
 
   security_rule {
-    name = "allow-https-priv-endpoint-443"
-    priority = 100
-    direction = "Outbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "443"
-    source_address_prefix = "*"
-    destination_address_prefix = var.address_prefix[2]
+    name                       = "allow-https-priv-endpoint-443"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = var.subnets["private_endpoints"].address_prefixes[0]
   }
 
   security_rule {
-    name = "allow-sql-priv-endpoint-1433"
-    priority = 110
-    direction = "Outbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "1433"
-    source_address_prefix = "*"
-    destination_address_prefix = var.address_prefix[2]
+    name                       = "allow-sql-priv-endpoint-1433"
+    priority                   = 110
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = var.subnets["private_endpoints"].address_prefixes[0]
   }
 }
 
 resource "azurerm_network_security_group" "vm_nsg" {
-  name = "vm_nsg" 
-  resource_group_name = azurerm_resource_group.azure_enterprise_project.name
-  location = azurerm_resource_group.azure_enterprise_project.location
+  for_each = var.subnets["vm"].nsg_enabled ? { vm = var.subnets["vm"] } : {}
+
+  name                = "vm_nsg"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = local.tags
 
   security_rule {
-    name = "allow-https-priv-endpoint-443"
-    priority = 200
-    direction = "Outbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "443"
-    source_address_prefix = "*"
-    destination_address_prefix = var.address_prefix[2]
+    name                       = "allow-https-priv-endpoint-443"
+    priority                   = 200
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = var.subnets["private_endpoints"].address_prefixes[0]
   }
 
   security_rule {
-    name = "allow-sql-priv-endpoint-1433"
-    priority = 210
-    direction = "Outbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "1433"
-    source_address_prefix = "*"
-    destination_address_prefix = var.address_prefix[2]
+    name                       = "allow-sql-priv-endpoint-1433"
+    priority                   = 210
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = var.subnets["private_endpoints"].address_prefixes[0]
   }
 
   security_rule {
-    name = "allow-ssh"
-    priority = 300
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "22"
-    source_address_prefix = var.address_prefix[4]
-    destination_address_prefix = var.address_prefix[4]
+    name                       = "allow-ssh"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.subnets["bastion"].address_prefixes[0]
+    destination_address_prefix = var.subnets["vm"].address_prefixes[0]
   }
 
   security_rule {
-    name = "allow-rdp"
-    priority = 310
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "3389"
-    source_address_prefix = var.address_prefix[4]
-    destination_address_prefix = var.address_prefix[0]
+    name                       = "allow-rdp"
+    priority                   = 310
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = var.subnets["bastion"].address_prefixes[0]
+    destination_address_prefix = var.subnets["vm"].address_prefixes[0]
   }
 
   security_rule {
-    name = "allow-80/443"
-    priority = 320
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_ranges = ["80", "443"]
-    source_address_prefix = var.address_prefix[3]
-    destination_address_prefix = var.address_prefix[0]
+    name                       = "allow-http-from-appgw-80"
+    priority                   = 320
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = var.subnets["appgw"].address_prefixes[0]
+    destination_address_prefix = var.subnets["vm"].address_prefixes[0]
   }
+
+  security_rule {
+    name                       = "allow-https-from-appgw-443"
+    priority                   = 330
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = var.subnets["appgw"].address_prefixes[0]
+    destination_address_prefix = var.subnets["vm"].address_prefixes[0]
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "app" {
+  for_each = var.subnets["app"].nsg_enabled ? { app = var.subnets["app"] } : {}
+
+  subnet_id                 = azurerm_subnet.this["app"].id
+  network_security_group_id = azurerm_network_security_group.app_nsg["app"].id
+}
+
+resource "azurerm_subnet_network_security_group_association" "vm" {
+  for_each = var.subnets["vm"].nsg_enabled ? { vm = var.subnets["vm"] } : {}
+
+  subnet_id                 = azurerm_subnet.this["vm"].id
+  network_security_group_id = azurerm_network_security_group.vm_nsg["vm"].id
 }
