f66f55bd0cedce16f42f0378d9c5b0490e9b475e
AJ Thomas <1ajthomas248@.com>
2026-06-10 14:19:35 -0500

    feat: wire root networking module

--- main.tf
+++ main.tf
@@ -13,6 +13,55 @@ terraform {
 }
 
 resource "azurerm_resource_group" "azure_enterprise_project" {
-  name = "azure_enterprise_project"
+  name     = "azure_enterprise_project"
   location = "East US"
+}
+
+module "networking" {
+  source = "./modules/networking"
+
+  resource_group_name = azurerm_resource_group.azure_enterprise_project.name
+  location            = azurerm_resource_group.azure_enterprise_project.location
+
+  common_tags = {
+    environment = "dev"
+    project     = "azure-enterprise"
+  }
+
+  vnet = {
+    name          = "enterprise-vnet"
+    address_space = ["10.0.0.0/16"]
+  }
+
+  subnets = {
+    app = {
+      name             = "app-subnet"
+      address_prefixes = ["10.0.1.0/24"]
+      nsg_enabled      = true
+    }
+
+    vm = {
+      name             = "vm-subnet"
+      address_prefixes = ["10.0.2.0/24"]
+      nsg_enabled      = true
+    }
+
+    private_endpoints = {
+      name             = "private-endpoints-subnet"
+      address_prefixes = ["10.0.3.0/24"]
+      nsg_enabled      = false
+    }
+
+    appgw = {
+      name             = "appgw-subnet"
+      address_prefixes = ["10.0.4.0/24"]
+      nsg_enabled      = false
+    }
+
+    bastion = {
+      name             = "AzureBastionSubnet"
+      address_prefixes = ["10.0.5.0/24"]
+      nsg_enabled      = false
+    }
+  }
 }
