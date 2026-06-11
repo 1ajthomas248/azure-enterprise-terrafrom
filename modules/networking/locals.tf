34e1100a825a227b32458f46bf3c1ad889dd51e5
AJ Thomas <1ajthomas248@.com>
2026-06-11 08:50:29 -0500

    feat(networking): add reusable resource name locals

--- modules/networking/locals.tf
+++ modules/networking/locals.tf
@@ -5,4 +5,13 @@ locals {
       managed_by = "terraform"
     }
   )
+
+  names = {
+    app_nsg     = "${var.name_prefix}-nsg-app"
+    vm_nsg      = "${var.name_prefix}-nsg-vm"
+    bastion     = "${var.name_prefix}-bastion"
+    bastion_pip = "${var.name_prefix}-pip-bastion"
+    appgw_pip   = "${var.name_prefix}-pip-appgw"
+    appgw       = "${var.name_prefix}-appgw-main"
+  }
 }
