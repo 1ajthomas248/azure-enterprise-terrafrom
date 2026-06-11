8162a327db08cb909130fa9876828807059f4063
AJ Thomas <1ajthomas248@.com>
2026-06-11 08:50:23 -0500

    refactor(networking): document inputs and add feature flags

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,17 +1,21 @@
 variable "resource_group_name" {
-  type = string
+  description = "Name of the resource group where the virtual network and subnets will be created"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "Location of the resource group and its resources"
+  type        = string
 }
 
 variable "common_tags" {
-  type    = map(string)
-  default = {}
+  description = "Common tags to be applied to all resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "vnet" {
+  description = "Virtual network configuration"
   type = object({
     name          = string
     address_space = list(string)
@@ -19,9 +23,33 @@ variable "vnet" {
 }
 
 variable "subnets" {
+  description = "Subnet configurations"
   type = map(object({
     name             = string
     address_prefixes = list(string)
     nsg_enabled      = bool
+
+    delegation = optional(object({
+      name                    = string
+      service_delegation_name = string
+      actions                 = list(string)
+    }))
   }))
+}
+
+variable "name_prefix" {
+  description = "Prefix for naming resources"
+  type        = string
+}
+
+variable "enable_bastion" {
+  description = "Whether to deploy Azure Bastion"
+  type        = bool
+  default     = true
+}
+
+variable "enable_application_gateway" {
+  description = "Whether to deploy Azure Application Gateway"
+  type        = bool
+  default     = true
 }
