3ff3d7eeb3c3817c64b8fe1e6685be7934a22701
AJ Thomas <1ajthomas248@.com>
2026-06-11 08:50:38 -0500

    feat(networking): add subnet delegation Bastion and WAF gateway

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -14,12 +14,25 @@ resource "azurerm_subnet" "this" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.enterprise_vnet.name
   address_prefixes     = each.value.address_prefixes
+
+  dynamic "delegation" {
+    for_each = each.value.delegation == null ? [] : [each.value.delegation]
+
+    content {
+      name = delegation.value.name
+
+      service_delegation {
+        name    = delegation.value.service_delegation_name
+        actions = delegation.value.actions
+      }
+    }
+  }
 }
 
 resource "azurerm_network_security_group" "app_nsg" {
   for_each = var.subnets["app"].nsg_enabled ? { app = var.subnets["app"] } : {}
 
-  name                = "app_nsg"
+  name                = local.names.app_nsg
   resource_group_name = var.resource_group_name
   location            = var.location
 
@@ -53,7 +66,7 @@ resource "azurerm_network_security_group" "app_nsg" {
 resource "azurerm_network_security_group" "vm_nsg" {
   for_each = var.subnets["vm"].nsg_enabled ? { vm = var.subnets["vm"] } : {}
 
-  name                = "vm_nsg"
+  name                = local.names.vm_nsg
   resource_group_name = var.resource_group_name
   location            = var.location
 
@@ -144,4 +157,111 @@ resource "azurerm_subnet_network_security_group_association" "vm" {
 
   subnet_id                 = azurerm_subnet.this["vm"].id
   network_security_group_id = azurerm_network_security_group.vm_nsg["vm"].id
+}
+
+resource "azurerm_public_ip" "bastion" {
+  count = var.enable_bastion ? 1 : 0
+
+  name                = local.names.bastion_pip
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = local.tags
+}
+
+resource "azurerm_bastion_host" "this" {
+  count = var.enable_bastion ? 1 : 0
+
+  name                = local.names.bastion
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  ip_configuration {
+    name                 = "default"
+    subnet_id            = azurerm_subnet.this["bastion"].id
+    public_ip_address_id = azurerm_public_ip.bastion[0].id
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_public_ip" "application_gateway" {
+  count = var.enable_application_gateway ? 1 : 0
+
+  name                = local.names.appgw_pip
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = local.tags
+}
+
+resource "azurerm_application_gateway" "this" {
+  count = var.enable_application_gateway ? 1 : 0
+
+  name                = local.names.appgw
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 1
+  }
+
+  gateway_ip_configuration {
+    name      = "appgw-ip-configuration"
+    subnet_id = azurerm_subnet.this["appgw"].id
+  }
+
+  frontend_ip_configuration {
+    name                 = "public"
+    public_ip_address_id = azurerm_public_ip.application_gateway[0].id
+  }
+
+  frontend_port {
+    name = "http"
+    port = 80
+  }
+
+  backend_address_pool {
+    name = "app-backend-pool"
+  }
+
+  backend_http_settings {
+    name                  = "http"
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 30
+  }
+
+  http_listener {
+    name                           = "http"
+    frontend_ip_configuration_name = "public"
+    frontend_port_name             = "http"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "http-to-app-backend"
+    priority                   = 100
+    rule_type                  = "Basic"
+    http_listener_name         = "http"
+    backend_address_pool_name  = "app-backend-pool"
+    backend_http_settings_name = "http"
+  }
+
+  waf_configuration {
+    enabled          = true
+    firewall_mode    = "Prevention"
+    rule_set_type    = "OWASP"
+    rule_set_version = "3.2"
+  }
+
+  tags = local.tags
 }
